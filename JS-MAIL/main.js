5e823146df6fc88151eda7b751849c1441527636
ruzica-glavas <ruzi99@hotmail.it>
2025-01-14 12:56:42 +0100

    add: finish mail javascript

--- JS-MAIL/main.js
+++ JS-MAIL/main.js
@@ -14,15 +14,49 @@ Non è necessario provvedere alla validazione delle email
 /*
 mini tasks:
     - creare un'array con 3-5 email
-    - creare un prompt dove chiedo all'utente la propria email
+    - creare un prompt dove chiedo all'utente la propria email 
     - creare un ciclo for con all'interno un console.log dove scrivo "l'email dell'utente é ${email}"
 
 */
 
-const emails = [
+
+//lista delle email
+
+const emailsInvitati = [
     'nathan@gmail.com',
     'ed@gmail.com',
     'fabio@gmail.com',
     'phil@gmail.com',
     'carlo@gmail.com'
 ]
+
+//prompt per l'utente
+
+const emailsPrompt = prompt ("Inserisci la tua email")
+
+/*dichiarare la variabile per verificare se l'email é presente nella lista*/
+
+let emailsPresent 
+
+//creare il ciclo for
+
+for (i=0; i<emailsInvitati.length; i++){
+    //se true
+
+    if(emailsInvitati[i]===emailsPrompt){
+        emailsPresent=true
+
+    // se false
+}else{
+    emailsPresent
+}
+
+}
+
+//Creare il messaggio appropriato in caso l'email sia o meno all'interno della lista di invitati
+
+if(emailsPresent){
+    console.log("La tua email é presente nella lista degli invitati")
+} else{
+    console.log("La tua email non é presente nella lista degli invitati")
+}
