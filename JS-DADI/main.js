bd4c25cc3746677f157251a4dc0b7303764c4224
ruzica-glavas <ruzi99@hotmail.it>
2025-01-14 15:58:36 +0100

    add: finish the Dadi exercise

--- JS-DADI/main.js
+++ JS-DADI/main.js
@@ -7,7 +7,7 @@ Stabilire il vincitore, in base a chi fa il punteggio più alto.
 /*
 mini tasks:
     - creare due array che vadano da 1 a 6 in modo randomico sia per il computer sia per il giocatore;
-    - fare un console.log dove viene mostrato i numeri pescati
+    - fare due console.log dove vengono mostrati i numeri pescati
     - chi estrae il numero più alto, vince (quindi creare una condizione)
 */
 
@@ -15,10 +15,18 @@ mini tasks:
 
 //Creazione array
 
-const computerDado = Math.random()*6
-const playerDado = Math.random()*6
+const computerDado = Math.round(Math.random()*6)
+const playerDado = Math.round (Math.random()*6)
 
-console.log(`Numero estratto dal computer: ${computerDado.toFixed(0)}.`)
-console.log(`Numero estratto dal giocatore: ${playerDado.toFixed(0)}`)
+console.log(`Numero estratto dal computer: ${computerDado}.`)
+console.log(`Numero estratto dal player: ${playerDado}`)
 
 //Creazione delle condizioni
+
+if(computerDado>playerDado){
+    console.log("Computer ha estratto il numero più alto e quindi vince")
+}else if(computerDado<playerDado){
+    console.log("Player ha estratto il numero più alto e quindi vince")
+} else{
+    console.log("Player e Computer hanno estratto lo stesso numero, quindi pareggio")
+}
